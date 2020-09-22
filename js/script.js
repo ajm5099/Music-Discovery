aac4dc2af676a510006cd64d1c98f4a3075d6f16
Evan K <EvanK311@gmail.com>
2020-09-22 13:50:36 -0700

    consolelog youtube APIviaAjax

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log("hello world")
+
+API = "AIzaSyChwdYAov09eDIzPKMuNd"
+
+
+
+$.ajax({
+    url: "https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=concert+Modest+Mouse&key="+ API +"-Aux-Il6N6alI",    
+    method: "GET"
+}).then(function (response) {
+    console.log(response)
+})
