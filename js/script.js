7cfc45c44374152312a76f09651c0b5b5cd124b4
Evan K <EvanK311@gmail.com>
2020-09-23 14:10:14 -0700

    resolving multipage issues

--- js/script.js
+++ js/script.js
@@ -1,44 +1,33 @@
+
+
 //=========================================================================================
 //Index script
 //=========================================================================================
-
+console.log(window.location.href)
 //variables
 console.log("hello world");
 var API = "AIzaSyChwdYAov09eDIzPKMuNd";
-
+var localBand = localStorage.getItem("band")
+console.log(localBand)
 
 
 
 //functions
 
-function getData(searchString) {
-
-    $.ajax({
-        url: `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&q=${searchString}+Modest+Mouse&key=${API}-Aux-Il6N6alI`,
-        method: "GET"
-    }).then(function (response) {
-
-        console.log(response)
-        $.ajax({
-            url: `https://www.theaudiodb.com/api/v1/json/1/search.php?s=${searchString}`,
-            method: "GET"
-        }).then(function (responseDescription) {
-            console.log(responseDescription)
-        })
-
-
-    })
-
-}
-
+// TODO: add timer and .then to create time between function call and search function to allow time to populate local var
 
 //events
 
-$("#search-button").on("click", function (event) {
-    
+$(".search-button").on("click", function (event) {
+    event.preventDefault()
     var searchString = $("#searchfield").val().trim();
     console.log(searchString)
-    getData(searchString);
+    localStorage.setItem("band", searchString)
+    setTimeout(function(){ window.location.href = "./mediaplayer.html" }, 500);
+    // window.location.href = "./mediaplayer.html"
+    
+
+    // getData(searchString);
 })
 
 //=========================================================================================
